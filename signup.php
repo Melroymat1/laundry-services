569cef97f3acd2818622954895ddd16a51555c5b
Melroymat1 <98328665+Melroymat1@users.noreply.github.com>
2024-03-04 15:45:19 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,11 +1,24 @@
 <?php
-$server = "localhost";
-$user = "root";
-$pass = "";
-$con = mysqli_connect($server, $user, $pass);
-
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$con = mysqli_connect($server, $username, $password);
 if (!$con) {
-    die("Error");
-
+    die("error");
 }
-echo "Successfull";
+echo "connection established";
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$number = $_POST['number'];
+$address = $_POST['address'];
+$pincode = $_POST['pincode'];
+
+$query = "INSERT INTO `admin2`.`signup1`(`name`, `email`, `password`, `number`, `address`, `pincode`) VALUES ('$name','$email','$password','$number','$address','$pincode')";
+
+$res = $con->query($query);
+if (!$res) {
+    die("not inserted");
+}
+echo "<script>alert('registration successfull')</script>";
